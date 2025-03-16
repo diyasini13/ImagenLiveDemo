1dd2419ec1fd0b69589161cda7ada92f2243db18
diyasini13 <diyasini@google.com>
2025-03-16 16:10:19 +0530

    added auth_tokentry.py

--- auth_token.py
+++ auth_token.py
@@ -27,9 +27,7 @@ def authentication():
             return "Error"
 
     # Get the token from the query parameter
-    token = st.experimental_get_query_params().get("token", [""])[0]
-    print("Token from exp: " , token)
-    
+    token = st.query_params.get("token", "")
     if not token:
         st.error("Unauthorized access. Token not provided.")
         st.stop()
